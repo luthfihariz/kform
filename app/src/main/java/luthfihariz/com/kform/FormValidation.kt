d401efd303f89287c2957b8cb213feb9f67c1b4b
Luthfi Hariz <luthfihariz@gmail.com>
2018-04-18 13:51:07 +0700

    Add support for text input layout

--- app/src/main/java/luthfihariz/com/kform/FormValidation.kt
+++ app/src/main/java/luthfihariz/com/kform/FormValidation.kt
@@ -1,16 +1,60 @@
 package luthfihariz.com.kform
 
+import android.support.design.widget.TextInputLayout
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Patterns
+import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 /**
  * Created by luthfihariz on 17/04/18.
  */
 
-fun EditText.validate(validator: (String) -> Boolean, message: String) {
-    if (!validator(text.toString())) {
-        error = message
+fun EditText.validate(validator: (String) -> Boolean, errorMessage: String, errorView: TextView? = null) {
+    val valid = validator(text.toString())
+    if (!valid) {
+        if (errorView != null) {
+            errorView.text = errorMessage
+        } else {
+            error = errorMessage
+        }
     }
+}
+
+fun TextInputLayout.validate(validator: (String) -> Boolean, errorMessage: String) {
+    if (!validator(editText!!.toString())) {
+        error = errorMessage
+    }
+}
+
+fun TextInputLayout.validateWhileTyping(validator: (String) -> Boolean, errorMessage: String) {
+    editText?.afterTextChanged {
+        validate(validator, errorMessage)
+    }
+}
+
+fun TextInputLayout.validateOutFocus(validator: (String) -> Boolean, errorMessage: String) {
+    editText?.setOnFocusChangeListener { _, isFocus ->
+        if (!isFocus) {
+            validate(validator, errorMessage)
+        }
+    }
+}
+
+fun EditText.afterTextChanged(func: (String) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+            func.invoke(p0.toString())
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+    })
 }
 
 fun String.isValidEmail() = isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
@@ -25,6 +69,7 @@ object Scheme {
     const val ALPHA_NUMERIC = ""
     const val ALPHA_MIXED_CASE = ""
     const val NUMERIC = ""
+    const val ALPHA_NUMERIC_MIXED_CASE = ""
 }
 
 fun String.matchScheme(scheme: String) = matches(Regex(scheme))
--- app/src/main/java/luthfihariz/com/kform/TestActivity.kt
+++ app/src/main/java/luthfihariz/com/kform/TestActivity.kt
@@ -15,8 +15,21 @@ class TestActivity : Activity() {
 
         submit.setOnClickListener {
             email.validate({ s -> s.isValidEmail() }, "Not a valid email")
-            fullname.validate({s -> s.minimum(3)}, "Should contain 3 characters at minimum")
-            password.validate({ s -> s.minimum(6) && s.matchScheme(Scheme.ALPHA_NUMERIC) }, "Password should minimum 6 character and alpha numeric")
+            fullname.validate({ s -> s.minimum(3) }, "Should contain 3 characters at minimum")
+            phoneNumber.validate({ s -> s.between(8, 13) }, "Should min 8 and max 13")
+            password.validate({
+                it.minimum(6) && it.matchScheme(Scheme.ALPHA_NUMERIC)
+            }, "Password should minimum 6 character and alpha numeric")
+            confirmPassword.validate({ s -> s == password.text.toString() }, "Should match with password")
+
+            tilSample.validate({ s -> s.maximum(10) }, "Should less than 10 characters")
+
+            tilSample.let { }
+
+            val isValid = tilSample.validate {
+                it.maximum(15)
+                it.isValidEmail()
+            }
         }
     }
 }
